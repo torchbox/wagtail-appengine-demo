ec4cef9303fa197fa68343445506c4778a9a5989
Tom Dyson <tom@torchbox.com>
2017-10-17 22:50:52 +0100

    Use base settings, don't include search URLs

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wagae.settings.dev")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wagae.settings.base")
 
     from django.core.management import execute_from_command_line
 
