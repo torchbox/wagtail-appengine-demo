4b33e205f1069a865237ed7a35012b94d371131e
Tom Dyson <tom@torchbox.com>
2017-10-17 21:59:38 +0100

    Import skeleton app

---
+++ README.md
@@ -0,0 +1,94 @@
+# Wagtail on Google App Engine demonstration
+
+This is the simplest possible Wagtail site on Google Cloud, using App Engine Flexible and Cloud SQL.
+
+## Assumptions
+
+1. A project has been created in the Google Cloud Platform Console
+2. Billing has been enabled for your project
+3. The Google Cloud SDK is installed locally
+4. The Google Cloud SQL API has been enabled for your project
+
+These steps are detailed in the [Google Cloud documentation](https://cloud.google.com/python/django/flexible-environment#before-you-begin).
+
+## Download this app and install its dependencies
+
+```
+git clone https://github.com/torchbox/wagtail-appengine-demo
+cd wagtail-appengine-demo
+pip install -r requirements.txt
+```
+
+## Prepare a Cloud SQL database
+
+Using the [Google Cloud instructions](https://cloud.google.com/python/django/flexible-environment#install_the_sql_proxy), install the Cloud SQL proxy, create a Cloud SQL instance (with PostgreSQL), create a database user called `wagtail_user` and a database called `wagtail`.
+
+Record the `Instance connection name` from the Overview tab of the Cloud SQL console.
+
+## Update your database settings
+
+Replace `<DB PASSWORD>` and `<INSTANCE CONNECTION NAME>` in `wagae/settings/base.py`.
+
+## Install the database tables
+
+1. Run Cloud SQL Proxy in a separate terminal:
+
+`cloud_sql_proxy -instances="<INSTANCE CONNECTION NAME>"=tcp:5432`
+
+2. Create Wagtail's database tables and an initial user:
+
+```
+./manage.py migrate
+./manage.py createsuperuser
+```
+
+## Check everything works locally
+
+`./manage.py runserver`
+
+You should be able to log in to the Wagtail admin interface at http://127.0.0.1:8000/admin/ with the user details you have just created.
+
+## Create a bucket for static content
+
+```
+gsutil mb gs://<GCS BUCKET NAME>
+gsutil defacl set public-read gs://<GCS BUCKET NAME>
+gsutil cors set cors.json gs://<GCS BUCKET NAME>
+```
+
+Update `STATIC_URL` in `wagae/settings/base.py`:
+
+```python
+STATIC_URL = 'https://storage.googleapis.com/<GCS BUCKET NAME>/static/'
+```
+
+and copy the files across:
+
+```
+gsutil rsync -R static/ gs://<GCS BUCKET NAME>/static
+```
+
+## Configure the app for App Engine
+
+Set `<INSTANCE CONNECTION NAME>` in `app.yaml`.
+
+In settings/base.py, update `<GCS BUCKET NAME>` and `<GCP PROJECT ID>` 
+and set `<YOUR SECRET KEY>`, e.g. using
+
+```python
+import random
+''.join([random.SystemRandom().choice(
+    'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+    ) for i in range(50)])
+```
+
+Deploy your site with `gcloud app deploy`.
+
+### Hardening
+
+1. Once you know the production domain for your site, specify this in `ALLOWED_HOSTS` and `BASE_URL`.
+1. Update the origin in cors.json and rerun `gsutil cors set cors.json`
+1. Google auth
+1. Search
+1. Email?
+1. SSG to Firebase
---
+++ wagae/settings/base.py
@@ -0,0 +1,118 @@
+from __future__ import absolute_import, unicode_literals
+import os
+
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(PROJECT_DIR)
+
+INSTALLED_APPS = [
+    'home',
+    'wagtail.wagtailforms',
+    'wagtail.wagtailredirects',
+    'wagtail.wagtailembeds',
+    'wagtail.wagtailsites',
+    'wagtail.wagtailusers',
+    'wagtail.wagtailsnippets',
+    'wagtail.wagtaildocs',
+    'wagtail.wagtailimages',
+    'wagtail.wagtailsearch',
+    'wagtail.wagtailadmin',
+    'wagtail.wagtailcore',
+    'modelcluster',
+    'taggit',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'storages',
+]
+
+MIDDLEWARE = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'wagtail.wagtailcore.middleware.SiteMiddleware',
+    'wagtail.wagtailredirects.middleware.RedirectMiddleware',
+]
+
+ROOT_URLCONF = 'wagae.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(PROJECT_DIR, 'templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'wagae.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wagtail',
+        'USER': 'wagtail_user',
+        'PASSWORD': '<DB PASSWORD>',
+        'PORT': '5432',
+    }
+}
+
+DATABASES['default']['HOST'] = '/cloudsql/<INSTANCE CONNECTION NAME>'
+if os.getenv('GAE_INSTANCE'):
+    pass
+else:
+    DATABASES['default']['HOST'] = '127.0.0.1'
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR, 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'https://storage.googleapis.com/<GCS BUCKET NAME>/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+DEBUG = False
+SECRET_KEY = '<YOUR SECRET KEY>'
+
+# Wagtail settings
+WAGTAIL_SITE_NAME = "Wagtail on App Engine"
+
+# Base URL to use when referring to full URLs within the Wagtail admin backend -
+# e.g. in notification emails. Don't include '/admin' or a trailing slash
+BASE_URL = 'http://example.com'
+ALLOWED_HOSTS = ['*']
+
+DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+GS_BUCKET_NAME = '<GCS BUCKET NAME>'
+GS_PROJECT_ID = '<GCP PROJECT ID>'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
